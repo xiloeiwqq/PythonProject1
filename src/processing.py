c4821d071ae830ce3a612d846b808aa8cc93ce41
xiloeiwqq <pspsibladessss@gmail.com>
2025-02-16 20:46:08 +0300

    были отредактированы некоторые функции и добавлены тесты к ним, а также изменен README.md файл

--- src/processing.py
+++ src/processing.py
@@ -9,9 +9,11 @@ def filter_by_state(data: List[Dict[str, Any]], state: str = "EXECUTED") -> List
     return [item for item in data if item.get("state") == state]
 
 
-def sort_by_date(data: List[Dict], descending: bool = True) -> List[Dict]:
+def sort_by_date(data: List[Dict[str, Any]], descending: bool = True) -> List[Dict[str, Any]]:
     """
     Сортирует список словарей по ключу 'date'.
     """
     return sorted(
         data, key=lambda x: datetime.fromisoformat(x["date"]) if x.get("date") else datetime.min, reverse=descending
+    )
+
--- src/widget.py
+++ src/widget.py
@@ -1,25 +1,34 @@
-def mask_account_card(card_info):
-    import re
+import re
+from datetime import datetime
 
-    def mask_card_number(number):
-        return number[:4] + ' ' + number[4:6] + '** **** ' + number[-4:]
 
-    def mask_account_number(number):
-        return '**' + number[-4:]
+def mask_card_number(number: str) -> str:
+    return number[:4] + " " + number[4:6] + "** **** " + number[-4:]
 
+
+def mask_account_number(number: str) -> str:
+    return "**" + number[-4:]
+
+
+def mask_account_card(card_info: str) -> str:
     card_pattern = re.compile(r"(Visa|Maestro) (\d{16})")
     account_pattern = re.compile(r"(Счет) (\d{20})")
 
-    if card_pattern.match(card_info):
-        card_type, card_number = card_pattern.match(card_info).groups()
+    card_match = card_pattern.match(card_info)
+    if card_match:
+        card_type, card_number = card_match.groups()
         return f"{card_type} {mask_card_number(card_number)}"
-    elif account_pattern.match(card_info):
-        account_type, account_number = account_pattern.match(card_info).groups()
-        return f"{account_type} {mask_account_number(account_number)}"
-    else:
-        return card_info
 
-def get_date(date_str):
-    from datetime import datetime
-    date_obj = datetime.strptime(date_str, "%Y-%m-%dT%H:%M:%S.%f")
-    return date_obj.strftime("%d.%m.%Y")
+    account_match = account_pattern.match(card_info)
+    if account_match:
+        account_type, account_number = account_match.groups()
+        return f"{account_type} {mask_account_number(account_number)}"
+
+    return card_info
+
+
+def get_date(date: str) -> str:
+    date_form = f"{date[8:10]+'.'+date[5:7]+'.'+date[2:4]}"
+    if "T" in date:
+        date = date.split("T")[0]
+    return date_form
