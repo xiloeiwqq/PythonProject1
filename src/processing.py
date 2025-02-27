ffb4caecc54c45e075270b4c3ff7c30fe4daa6c7
xiloeiwqq <pspsibladessss@gmail.com>
2025-02-27 23:26:03 +0300

    .

--- src/processing.py
+++ src/processing.py
@@ -1,5 +1,5 @@
 from datetime import datetime
-from typing import List, Dict, Any
+from typing import Any, Dict, List
 
 
 def filter_by_state(data: List[Dict[str, Any]], state: str = "EXECUTED") -> List[Dict[str, Any]]:
